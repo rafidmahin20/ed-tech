1dfc6d8426e7a88897056d1ed85cfceccd78b863
rafidmahin20 <submanmahin@gmail.com>
2022-08-20 00:55:32 +0600

    edited

--- apple/src/App.js
+++ apple/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div>
     <Header></Header>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
     </div>
   );
 }
--- apple/src/Components/Home/Banner.js
+++ apple/src/Components/Home/Banner.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Banner = () => {
     return (
-        <div>
-            
+        <div className='banner' style={{
+            backgroundImage: "url(https://i.ibb.co/TMkFKRK/sergey-zolkin-Ue-Y8a-TI6d0-unsplash.png)",
+            height: '400px'
+        }}>
+            <div className='banner-0verlay bg-opacity-20'>
+            <div className='lg:mr-auto lg:ml-24'>
+                <div className="banner-content text-center  text-white">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold uppercase text-primary">Macbook Pro</h1>
+                        <p className='text-xl font-normal'>Macbook
+                            Powered By M1 Chip. <br /> Your Daily Driver</p>
+                        <p className="mb-5"></p>
+                        <Link to='/service'><button className="btn bg-red-600 rounded-full p-3">Shop Now</button></Link>
+                    </div>
+                </div>
+            </div>
+            </div>
         </div>
     );
 };
